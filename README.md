4ce30f2a29b2ce7cf584faefeb1f6c4c4c901551
ReverbDesigns <jordymcbell@gmail.com>
2020-08-08 13:56:42 -0700

    first commit+ files

---
+++ README.md
@@ -0,0 +1,19 @@
+Password Generator assignment 3 BCS
+
+Meets all criteria
+
+i.e.
+
+You will be selecting which criteria to include in the password.
+
+You will be selecting the length of the password.
+
+You may choose lowercase, uppercase, numeric, and/or special characters.
+
+You will be selecting the length of the password.
+
+hosted version: https://reverbdesigns.github.io/passwordgen
+
+git repo: https://github.com/ReverbDesigns/passwordgen.git
+
+ ![index screenshot](./assets/images/index1.png)
---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// questions *****************************************************
+var questions = [
+    {
+        title: "Which of the following is true about cookie handling in JavaScript?Commonly Used data types DO NOT include:",
+        choices: ["JavaScript can manipulate cookies using the cookie property of the Document object.stings", "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.", "Both of the Above", "None Of the Above"],
+        answer: "Both of the Above"
+    },
+    {
+        title: "Which built-in method combines the text of two strings and returns a new string?",
+        choices: ["append()", "concat()", "attatch", "None of the Above"],
+        answer: "concat()"
+    },
+    {
+        title: "Which of the following function of String object returns the calling string value converted to lower case?",
+        choices: ["toLocaleLowerCase()", "toLowerCase()", "toString()", "substring()"],
+        answer: "toLowerCase()"
+    },
+    {
+        title: "Which of the following function of Array object joins all elements of an array into a string?",
+        choices: ["concat()", "join()", "pop()", "map()"],
+        answer: "join()"
+    },
+    {
+        title: "Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?",
+        choices: ["unshift()", "sort()", "splice()", "toString()"],
+        answer: "unshift()"
+    }
+]
